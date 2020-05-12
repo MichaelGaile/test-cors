ade912c3140671ed87fef84314cdcb4501ad5e4d
Michael <babusha9522gmail.com>
2020-05-12 16:36:29 +0300

    Updates

---
+++ precache-manifest.77ce49a82dc8ef6fc41bf02ec0d04b93.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc44a487889a2beefce02af168375330",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d0bcf76b17ad1445d17",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "c461260fddd0538ea967",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8d0bcf76b17ad1445d17",
+    "url": "/static/js/2.b3bfd20e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.b3bfd20e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c461260fddd0538ea967",
+    "url": "/static/js/main.96dbe56b.chunk.js"
+  },
+  {
+    "revision": "769f98eeb63e3e6f09b1",
+    "url": "/static/js/runtime-main.477b23d4.js"
+  }
+]);
